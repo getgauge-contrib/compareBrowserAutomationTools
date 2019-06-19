1d4c576f9e8ece11198c54ada009c8d2778f3c29
sswaroop <sswaroop@IN-sswaroop.local>
2019-06-19 11:55:19 +0530

    updated taiko tests

--- todomvc/taiko/tests/activities.js
+++ todomvc/taiko/tests/activities.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const { openBrowser,closeBrowser, goto, checkBox, click, near,waitFor,
+const { openBrowser,closeBrowser, goto, checkBox, click, near,into,$,inputField,hover,button,
     write, text, press } = require('taiko');
 
 describe('test todo list', () => {
@@ -23,18 +23,7 @@ describe('test todo list', () => {
     });
 
     it('should mark task as complete', async () => {
-        await write('Complete Task');
-        await press('Enter')
-        assert.ok(await text('1 item left').exists());
-        await click(checkBox(near('Complete Task')))
-        assert.ok(await text('0 items left').exists());
-
-        await click($("#toggle-all"));
-        await click("Clear completed");
-    });
-
-    it('should list active tasks', async () => {
-        await write('Complete Task');
+        await write('Complete Task',into(inputField({placeholder:"What needs to be done?"})));
         await press('Enter')
         assert.ok(await text('1 item left').exists());
         await click('Active')
@@ -44,53 +33,54 @@ describe('test todo list', () => {
         assert.ok(await text('0 items left').exists());
 
         await click('Active')
-        assert.ok(await !text('Complete Task').exists());
-
-        await click($("#toggle-all"));
-        await click("Clear completed");
+        assert.ok(!(await text('Complete Task').exists()));
     });
 
-    it('should list completed tasks', async () => {
-        await write('Completed Task');
+    it('should list active tasks', async () => {
+        await write('Completed Task',into(inputField({placeholder:"What needs to be done?"})));
         await press('Enter')
         assert.ok(await text('1 item left').exists());
-        await click('Completed')
-        assert.ok(await !text('Completed Task').exists());
-        await click('All')
+        await click('Active')
+        assert.ok(await text('Completed Task').exists());
 
         await click(checkBox(near('Completed Task')))
         assert.ok(await text('0 items left').exists());
 
-        await click('Completed')
-        assert.ok(await text('Completed Task').exists());
+        await click('Active')
+        assert.ok(!(await text('Completed Task').exists()));
+    });
 
-        await click($("#toggle-all"));
-        await click("Clear completed");
+    it('should list completed tasks', async () => {
+        await write('Complete Task',into(inputField({placeholder:"What needs to be done?"})));
+        await press('Enter')
+        assert.ok(await text('1 item left').exists());
+        await click('Active')
+        assert.ok(await text('Complete Task').exists());
+
+        await click(checkBox(near('Complete Task')))
+        assert.ok(await text('0 items left').exists());
+
+        await click('Active')
+        assert.ok(!(await text('Complete Task').exists()));
     });
 
     it('should clear completed tasks', async () => {
-        await write('Clear Task');
+        await write('Clear Task',into(inputField({placeholder:"What needs to be done?"})));
         await press('Enter')
         assert.ok(await text('1 item left').exists());
         await click(checkBox(near('Clear Task')))
         assert.ok(await text('0 items left').exists());
 
         await click('Clear completed')
-        assert.ok(await !text('Clear Task').exists());
-
-        await click($("#toggle-all"));
-        await click("Clear completed");
+        assert.ok(!(await text('Clear Task').exists()));
     });
 
     it('should remove task', async () => {
-        await write('Remove Task');
+        await write('Remove Task',into(inputField({placeholder:"What needs to be done?"})));
         await press('Enter')
         assert.ok(await text('1 item left').exists());
         await hover("Remove Task");
         await click(button({class:"destroy"}));
-        assert.ok(await !text('Remove Task').exists());
-
-        await click($("#toggle-all"));
-        await click("Clear completed");
+        assert.ok(!(await text('Remove Task').exists()));
     });
 });
