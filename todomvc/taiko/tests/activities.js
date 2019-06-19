8e82bd8ec0af34c760cecaceba9d3a075bce5c83
sswaroop <sswaroop@IN-sswaroop.local>
2019-06-19 10:28:29 +0530

    added more tests

--- todomvc/taiko/tests/activities.js
+++ todomvc/taiko/tests/activities.js
@@ -13,14 +13,69 @@ describe('test todo list', () => {
         await closeBrowser();
     });
 
-    it('should clear completed tasks', async () => {
-        await write('Demo');
+    it('should add task', async () => {
+        await write('Add Task');
         await press('Enter')
         assert.ok(await text('1 item left').exists());
-        await click(checkBox(near('Demo')))
-        assert.ok(await text('0 items left').exists());
 
+        await click(label({class:"toggle-all"}))
         await click('Clear completed')
     });
 
+    it('should mark task as complete', async () => {
+        await write('Complete Task');
+        await press('Enter')
+        assert.ok(await text('1 item left').exists());
+        await click(checkBox(near('Complete Task')))
+        assert.ok(await text('0 items left').exists());
+    });
+
+    it('should list active tasks', async () => {
+        await write('Complete Task');
+        await press('Enter')
+        assert.ok(await text('1 item left').exists());
+        await click('Active')
+        assert.ok(await text('Complete Task').exists());
+
+        await click(checkBox(near('Complete Task')))
+        assert.ok(await text('0 items left').exists());
+
+        await click('Active')
+        assert.ok(await !text('Complete Task').exists());
+    });
+
+    it('should list completed tasks', async () => {
+        await write('Completed Task');
+        await press('Enter')
+        assert.ok(await text('1 item left').exists());
+        await click('Completed')
+        assert.ok(await !text('Completed Task').exists());
+        await click('All')
+
+        await click(checkBox(near('Completed Task')))
+        assert.ok(await text('0 items left').exists());
+
+        await click('Completed')
+        assert.ok(await text('Completed Task').exists());
+    });
+
+    it('should clear completed tasks', async () => {
+        await write('Clear Task');
+        await press('Enter')
+        assert.ok(await text('1 item left').exists());
+        await click(checkBox(near('Clear Task')))
+        assert.ok(await text('0 items left').exists());
+
+        await click('Clear completed')
+        assert.ok(await !text('Clear Task').exists());
+    });
+
+    it('should remove task', async () => {
+        await write('Remove Task');
+        await press('Enter')
+        assert.ok(await text('1 item left').exists());
+        await hover("Remove Task");
+        await click(button({class:"destroy"}));
+        assert.ok(await !text('Remove Task').exists());
+    });
 });
