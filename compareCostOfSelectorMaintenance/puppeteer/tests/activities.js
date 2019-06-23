3a32ee56a077001e54ae90af3c199115ad80c8f7
sswaroop <sswaroop@IN-sswaroop.local>
2019-06-24 03:42:21 +0530

    puppeteer activities

---
+++ compareCostOfSelectorMaintenance/puppeteer/tests/activities.js
@@ -0,0 +1,90 @@
+const assert = require('assert');
+const puppeteer = require('puppeteer');
+
+describe('test todo mvc activities', () => {
+
+    before(async () => {
+        browser = await puppeteer.launch({headless: false,args:['--window-size=1440,900']});
+        page = await browser.newPage();
+        await page.setViewport({width:1440,height:900});
+        const [response] = await Promise.all([
+            page.waitForNavigation(), // The promise resolves after navigation has finished
+            page.goto('http://todomvc.com/examples/react/#/'),
+          ]);
+    });
+
+    after(async () => {
+        await browser.close();
+    });
+
+    function delay(timeout) {
+        return new Promise((resolve) => {
+          setTimeout(resolve, timeout);
+        });
+      }
+      
+    it('should clear completed tasks',async () => {
+        await page.waitForSelector('input.new-todo')
+
+        await page.type('input.new-todo','Demo');
+        await page.type('input.new-todo',String.fromCharCode(13));
+
+        var newElements = (await page.$$('ul.todo-list>li'))
+        assert.ok(newElements.length==1)
+
+        await page.click('input.toggle');
+
+        var newElements = (await page.$$('ul.todo-list>li.completed'))
+        assert.ok(newElements.length==1)
+
+        await page.click('button.clear-completed');
+        var completedElements = (await page.$$('ul.todo-list>li'))
+        assert.ok(completedElements.length==0)
+  
+      });
+
+    it('should mark task as complete', async () => {
+      await page.waitForSelector('input.new-todo')
+
+      await page.focus('input.new-todo')
+      await page.type('input.new-todo','Complete Task');
+      await page.type('input.new-todo',String.fromCharCode(13));
+
+      var newElements = (await page.$$('ul.todo-list>li'))
+      assert.ok(newElements.length==1)
+
+      await page.click('input.toggle');
+
+      var newElements = (await page.$$('ul.todo-list>li.completed'))
+      assert.ok(newElements.length==1)
+
+      await page.click('button.clear-completed');
+      var completedElements = (await page.$$('ul.todo-list>li'))
+      assert.ok(completedElements.length==0)
+    });
+
+    it('should list active tasks', async () => {
+      await page.waitForSelector('input.new-todo')
+
+      await page.focus('input.new-todo')
+      await page.type('input.new-todo','Active Task');
+      await page.type('input.new-todo',String.fromCharCode(13));
+
+      await page.focus('input.new-todo')
+      await page.type('input.new-todo','Completed Task');
+      await page.type('input.new-todo',String.fromCharCode(13));
+
+      var newElements = (await page.$$('ul.todo-list>li'))
+      assert.ok(newElements.length==2)
+
+      await page.click('input.toggle');
+      assert.ok((await page.$$('ul.todo-list>li.completed')).length==1)
+
+      const [element] = await page.$x("//a[text()='Active']");
+      await element.click();
+
+      var activeElements = (await page.$$('ul.todo-list>li'))
+      assert.ok(activeElements.length==1)
+    });
+
+});
--- compareCostOfSelectorMaintenance/selenium/tests/activities.js
+++ compareCostOfSelectorMaintenance/selenium/tests/activities.js
@@ -33,43 +33,43 @@ describe ('test todo mvc activities', async function () {
         assert.ok((await webpage.findAllByXPath('//ul[@class="todo-list"]/li')).length==0)
     });
 
-    // it('should mark task as complete', async () => {
-    //     await driver.sleep(1000);
-    //     (await webpage.findByCss('input[placeholder="What needs to be done?"]')).sendKeys('Compelete Task');
-    //     await driver.sleep(1000);        
-    //     (await webpage.findByCss('input[placeholder="What needs to be done?"]')).sendKeys(webdriver.Key.ENTER);
-    //     await driver.sleep(1000);        
-    //     var newElements = (await webpage.findAllByXPath('//ul[@class="todo-list"]/li[@class=""]'))
-    //     assert.ok(newElements.length==1)
+    it('should mark task as complete', async () => {
+        await driver.sleep(1000);
+        (await webpage.findByCss('input[placeholder="What needs to be done?"]')).sendKeys('Compelete Task');
+        await driver.sleep(1000);        
+        (await webpage.findByCss('input[placeholder="What needs to be done?"]')).sendKeys(webdriver.Key.ENTER);
+        await driver.sleep(1000);        
+        var newElements = (await webpage.findAllByXPath('//ul[@class="todo-list"]/li[@class=""]'))
+        assert.ok(newElements.length==1)
 
-    //     await (await webpage.findByXPath('//input[@class="toggle"]')).click();
-    //     var completedElements = (await webpage.findAllByXPath('//ul[@class="todo-list"]/li[@class="completed"]'))
-    //     assert.ok(completedElements.length==1)
+        await (await webpage.findByXPath('//input[@class="toggle"]')).click();
+        var completedElements = (await webpage.findAllByXPath('//ul[@class="todo-list"]/li[@class="completed"]'))
+        assert.ok(completedElements.length==1)
 
-    //     await (await webpage.findByXPath('//button[@class="clear-completed"]')).click();
-    //     assert.ok((await webpage.findAllByXPath('//ul[@class="todo-list"]/li')).length==0)
-    // });
+        await (await webpage.findByXPath('//button[@class="clear-completed"]')).click();
+        assert.ok((await webpage.findAllByXPath('//ul[@class="todo-list"]/li')).length==0)
+    });
   
-    // it('should list active tasks', async () => {
-    //     await driver.sleep(1000);
-    //     (await webpage.findByCss('input[placeholder="What needs to be done?"]')).sendKeys('Active Task');
-    //     await driver.sleep(1000);        
-    //     (await webpage.findByCss('input[placeholder="What needs to be done?"]')).sendKeys(webdriver.Key.ENTER);
+    it('should list active tasks', async () => {
+        await driver.sleep(1000);
+        (await webpage.findByCss('input[placeholder="What needs to be done?"]')).sendKeys('Active Task');
+        await driver.sleep(1000);        
+        (await webpage.findByCss('input[placeholder="What needs to be done?"]')).sendKeys(webdriver.Key.ENTER);
 
-    //     await driver.sleep(1000);
-    //     (await webpage.findByCss('input[placeholder="What needs to be done?"]')).sendKeys('Completed Task');
-    //     await driver.sleep(1000);        
-    //     (await webpage.findByCss('input[placeholder="What needs to be done?"]')).sendKeys(webdriver.Key.ENTER);
-    //     await driver.sleep(1000);        
+        await driver.sleep(1000);
+        (await webpage.findByCss('input[placeholder="What needs to be done?"]')).sendKeys('Completed Task');
+        await driver.sleep(1000);        
+        (await webpage.findByCss('input[placeholder="What needs to be done?"]')).sendKeys(webdriver.Key.ENTER);
+        await driver.sleep(1000);        
 
-    //     var newElements = (await webpage.findAllByXPath('//ul[@class="todo-list"]/li[@class=""]'))
-    //     assert.ok(newElements.length==2)
+        var newElements = (await webpage.findAllByXPath('//ul[@class="todo-list"]/li[@class=""]'))
+        assert.ok(newElements.length==2)
 
-    //     await (await webpage.findByXPath('//input[@class="toggle"]')).click();
-    //     assert.ok((await webpage.findAllByXPath('//ul[@class="todo-list"]/li[@class="completed"]')).length==1)
-    //     await (await webpage.findByXPath('//a[contains(@href,"#/active")]')).click();
-    //     assert.ok((await webpage.findAllByXPath('//ul[@class="todo-list"]/li[@class=""]')).length==1)
-    // });
+        await (await webpage.findByXPath('//input[@class="toggle"]')).click();
+        assert.ok((await webpage.findAllByXPath('//ul[@class="todo-list"]/li[@class="completed"]')).length==1)
+        await (await webpage.findByXPath('//a[contains(@href,"#/active")]')).click();
+        assert.ok((await webpage.findAllByXPath('//ul[@class="todo-list"]/li[@class=""]')).length==1)
+    });
   
 //     it('should clear completed tasks', async () => {
 //     });
